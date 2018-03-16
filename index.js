2c56d3df95ff5f675e9d7d23aa7f523d84b6a78c
McBaconator5000 <ruschaaron@gmail.com>
2018-03-16 10:20:14 -0500

    Added ?set
    Gave ability to set avatar. Had to make a basic command in index.js as it has access to bot.user.setAvatar

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const secrets = require('./secret.json');
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
+const fs = require('fs');
+
+function checkFileExistsSync(filepath){
+    let flag = true;
+    try{
+      fs.accessSync(filepath, fs.F_OK);
+    }catch(e){
+      flag = false;
+    }
+    return flag;
+  }
 
 //start listener for discord
 //listen for !wtp (random fusion)
@@ -12,5 +23,41 @@ const bot = new commando.Client();
 bot.registry.registerGroup('fusion', 'Fusion');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
+
+
+//have to do set avatar here for some reason????
+bot.on('message', (message) => {
+    //use a different command starter "?" so the !help warning isn't triggered
+    if(message.content.startsWith("?set")){
+        rawSetInfo = message.content.slice(message.content.indexOf(" "), message.content.length).toLowerCase().trim(); //remove command string and set to lowercase
+        body = rawSetInfo.slice(0, rawSetInfo.indexOf(" ")); 
+        head = rawSetInfo.slice(rawSetInfo.indexOf(" ") + 1, rawSetInfo.length);
+
+        //get numbers if not numbers
+        if(isNaN(head)){
+            var headNumber = pokemon[head];
+        }else{
+            var headNumber = head;
+        }
+        if(isNaN(body)){
+           var bodyNumber = pokemon[body];
+        }else{
+            var bodyNumber = body;
+        }
+
+        //test to see if we already have the image saved
+        if(checkFileExistsSync('./images/'+headNumber+'X'+bodyNumber+'X0.png')){
+            bot.user.setAvatar('./images/'+headNumber+'X'+bodyNumber+'X0.png')
+            .then( user => {
+                message.channel.send("How do you like me new look?");
+            })
+            .catch(err => {
+                message.channel.send("You might be changing my avatar too fast. Try again in a bit.");
+            });//possible to message.send errors?
+        }else{
+            message.channel.send("I couldn't find the file. Run '!fuse " + headNumber + " " + bodyNumber + "' and then try again.")
+        }
+    }
+});
 
 bot.login(secrets.token);
